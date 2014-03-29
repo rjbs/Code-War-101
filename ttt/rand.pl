d3b39adeaf550528b9c6fda8b6549b2e34112a7e
Ricardo Signes <rjbs@cpan.org>
2014-03-28 21:52:45 -0400

    stop when stdin dries up

--- ttt/rand.pl
+++ ttt/rand.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl
 use 5.12.0;
 $|++;
-while (1) {
-  my $line = <STDIN>;
+while (my $line = <STDIN>) {
   $line =~ s/\R\z//;
   my ($me, $board) = $line =~ /\A(.) ([ 0-9]{9})$/;
   my @indices = grep { ' ' eq substr $board, $_, 1 } (0 .. 8);
